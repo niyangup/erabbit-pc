2dc5b2a741ba0337370f539445c9aa18ed0b4a30
poper <poper@wangxutech.com>
2022-08-09 14:22:07 +0800

    项目结构初始化

--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,19 @@
 import { createStore } from 'vuex'
+import user from '@/store/modules/user'
+import cart from '@/store/modules/cart'
+import category from '@/store/modules/category'
+import createPersistedState from 'vuex-persistedstate'
 
 export default createStore({
-  state: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
   modules: {
-  }
+    user,
+    cart,
+    category
+  },
+  plugins: [
+    createPersistedState({
+      key: 'erabbit-pc-store',
+      paths: ['user', 'cart']
+    })
+  ]
 })
