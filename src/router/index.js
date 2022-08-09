5b8b860912d4bc947bf3f32a248cf672f2651f91
poper <poper@wangxutech.com>
2022-08-09 21:13:23 +0800

    路由初始化

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,18 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
+import Layout from '@/views/Layout'
+import Home from '@/views/home'
 
 const routes = [
+  {
+    path: '/',
+    component: Layout,
+    children: [
+      {
+        path: '/',
+        component: Home
+      }
+    ]
+  }
 ]
 
 const router = createRouter({
---
+++ src/views/Layout.vue
@@ -0,0 +1,18 @@
+<template>
+  <nav>顶部</nav>
+  <header>头部</header>
+  <div class="main">
+    <router-view></router-view>
+  </div>
+  <footer>底部</footer>
+</template>
+
+<script>
+export default {
+  name: 'Layout'
+}
+</script>
+
+<style scoped>
+
+</style>
