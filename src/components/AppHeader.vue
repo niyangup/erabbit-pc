b047708673851a19f7b645d27ff57c33d1cf4157
poper <poper@wangxutech.com>
2022-08-09 22:39:34 +0800

    header footer组件完成

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,111 @@
+<template>
+  <div class="container-header">
+    <div class="container">
+      <h1>
+        <img src="@/assets/images/logo.png" alt="" class="logo">
+      </h1>
+      <ul>
+        <li>首页</li>
+        <li>居家</li>
+        <li>美食</li>
+        <li>美食</li>
+        <li>美食</li>
+        <li>美食</li>
+        <li>美食</li>
+        <li>美食</li>
+        <li>美食</li>
+        <li>美食</li>
+      </ul>
+      <div class="input">
+        <i class="iconfont icon-search"></i>
+        <input type="text" placeholder="搜一搜">
+      </div>
+
+      <div class="cart">
+        <i class="iconfont icon-cart"></i>
+        <span class="count">0</span>
+      </div>
+    </div>
+  </div>
+
+</template>
+
+<script>
+export default {
+  name: 'AppHeader'
+}
+</script>
+
+<style scoped lang="less">
+@height: 132px;
+.container-header {
+  height: @height;
+  background: white;
+
+  .container {
+    display: flex;
+    align-items: center;
+
+    .logo {
+      width: 200px;
+      height: @height;
+      object-fit: contain;
+    }
+
+    ul {
+      padding-left: 40px;
+      flex: 1;
+      display: flex;
+      font-size: 16px;
+      color: #333;
+
+      li {
+        margin-right: 40px;
+      }
+    }
+
+    .input {
+      position: relative;
+      border-bottom: 1px solid #e7e7e7;
+
+      i {
+        position: absolute;
+        font-size: 18px;
+        left: 5px;
+        top: 7px;
+        line-height: 18px;
+      }
+
+      input {
+        width: 170px;
+        height: 32px;
+        padding-left: 30px;
+      }
+    }
+
+    .cart {
+      width: 50px;
+      height: 32px;
+      position: relative;
+
+      i {
+        display: block;
+        font-size: 22px;
+      }
+
+      span {
+        font-size: 12px;
+        position: absolute;
+        right: 16px;
+        top: 0;
+        padding: 0 6px;
+        background: #e26237;
+        color: white;
+        border-radius: 10px;
+      }
+    }
+  }
+
+}
+
+</style>
--- src/views/Layout.vue
+++ src/views/Layout.vue
@@ -1,21 +1,27 @@
 <template>
   <AppNavBar></AppNavBar>
-  <header>头部</header>
+  <AppHeader></AppHeader>
   <div class="main">
     <router-view></router-view>
     <button @click="login">登录</button>
     <button @click="logout">退出</button>
   </div>
-  <footer>底部</footer>
+  <AppFooter></AppFooter>
 </template>
 
 <script>
 import AppNavBar from '@/components/AppNavBar'
 import { useStore } from 'vuex'
+import AppHeader from '@/components/AppHeader'
+import AppFooter from '@/components/AppFooter'
 
 export default {
   name: 'Layout',
-  components: { AppNavBar },
+  components: {
+    AppHeader,
+    AppNavBar,
+    AppFooter
+  },
   setup () {
     const store = useStore()
 
