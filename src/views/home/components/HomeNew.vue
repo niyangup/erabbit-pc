3be5da77e823ddc55da05250969e3a0d6d140e19
poper <poper@wangxutech.com>
2022-08-11 13:34:25 +0800

    人气推荐

---
+++ src/views/home/components/HomeNew.vue
@@ -0,0 +1,72 @@
+<template>
+  <div class="home-new">
+    <HomePanel title="新鲜好物" sub-title="新鲜出炉 品质靠谱">
+      <template #right>
+        <XtxMore path="/"/>
+      </template>
+      <div ref="target" style="position: relative;height: 426px;">
+        <!-- 面板内容 -->
+        <ul v-if="goods.length" class="goods-list">
+          <li v-for="item in goods" :key="item.id">
+            <RouterLink :to="`/product/${item.id}`">
+              <img :src="item.picture" alt="">
+              <p class="name ellipsis">{{ item.name }}</p>
+              <p class="price">&yen;{{ item.price }}</p>
+            </RouterLink>
+          </li>
+        </ul>
+        <!--        <HomeSkeleton bg="#f0f9f4" v-else/>-->
+      </div>
+    </HomePanel>
+  </div>
+</template>
+
+<script>
+import HomePanel from '@/views/home/components/HomePanel'
+import { ref } from 'vue'
+import { findNew } from '@/api/home'
+
+export default {
+  name: 'HomeNew',
+  components: {
+    HomePanel
+  },
+  setup () {
+    const goods = ref([])
+    findNew().then(value => {
+      goods.value = value.result
+    })
+    return { goods }
+  }
+}
+</script>
+
+<style scoped lang="less">
+.goods-list {
+  display: flex;
+  justify-content: space-between;
+  height: 406px;
+
+  li {
+    width: 306px;
+    height: 406px;
+    background: #f0f9f4;
+    .hoverShadow();
+
+    img {
+      width: 306px;
+      height: 306px;
+    }
+
+    p {
+      font-size: 22px;
+      padding: 12px 30px 0 30px;
+      text-align: center;
+    }
+
+    .price {
+      color: @priceColor;
+    }
+  }
+}
+</style>
--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -5,15 +5,22 @@
       <HomeBanner></HomeBanner>
     </div>
   </div>
+  <HomeNew></HomeNew>
+  <HomeHot></HomeHot>
 </template>
 
 <script>
 import HomeCategory from '@/views/home/components/HomeCategory'
 import HomeBanner from '@/views/home/components/HomeBanner'
+import HomeNew from '@/views/home/components/HomeNew'
+import HomeHot from '@/views/home/components/HomeHot'
 
 export default {
   name: 'Home',
   components: {
+    HomeHot,
+    // eslint-disable-next-line vue/no-unused-components
+    HomeNew,
     HomeBanner,
     HomeCategory
   }
@@ -26,6 +33,7 @@ export default {
   height: @height;
 
   .home-category {
+    height: @height;
     z-index: 100;
   }
 }
